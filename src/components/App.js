4fc879b236061da258bfdeb79a048b927f61c767
Garrett Brown <garrettpaulbrown@gmail.com>
2021-08-26 20:57:17 -0700

    Add header fragment.

---
+++ src/components/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Header from "./Header";
+
+function App(){
+  return (
+    <React.Fragment>
+      <Header />
+      <h2>Classic Arcade and Kombucha on Tap</h2>
+    </React.Fragment>
+  );
+}
+
+export default App;
